a16bfd417d4d74f3b4fee362f1a4d595e1293f03
siennatariman <tariman.sienna@gmail.com>
2024-01-06 21:25:40 +0800

    first commit

---
+++ chapter-1/HelloWorld.java
@@ -0,0 +1,12 @@
+
+/**
+ * Write a description of sample here.
+ * 
+ * @author (sienna_Tariman) 
+ * @version (01/06/24)
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+    }
+}
